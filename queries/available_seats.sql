da488e4e3e61b450402c319c75cb1f426aeaadec
Pujitha Medasani <mpujitha676@gmail.com>
2025-07-16 00:34:13 +0530

    Update available_seats.sql

--- queries/available_seats.sql
+++ queries/available_seats.sql
@@ -3,3 +3,5 @@
 SELECT seat_number
 FROM Seats
 WHERE flight_id = 1 AND is_booked = FALSE;
+
+-- This query retrieves all unbooked seat numbers for a specific flight (i.e., flight with ID = 1). It helps identify which seats are currently available for booking.
