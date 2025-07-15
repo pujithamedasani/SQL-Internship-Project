cf216a728eae47afaee96721c5735f23753ca25a
Pujitha Medasani <mpujitha676@gmail.com>
2025-07-16 00:27:02 +0530

    Update insert_data.sql

--- insert_data.sql
+++ insert_data.sql
@@ -1,14 +1,19 @@
 -- Flights Table
 INSERT INTO Flights (flight_number, origin, destination, departure_time, arrival_time) VALUES 
 ('AI101', 'New York', 'London', '2025-08-01 10:00:00', '2025-08-01 20:00:00'),
-('AI202', 'London', 'Paris', '2025-08-21 08:00:00', '2025-08-21 09:30:00');
+('AI202', 'London', 'Paris', '2025-08-21 08:00:00', '2025-08-21 09:30:00'),
+('AI203', 'India', 'United States of America' , '2025-09-12 08:20:00', '2025-09-13 05:40:00');
 
 -- Customers Table
 INSERT INTO Customers (full_name, email, phone) VALUES 
-('Saachi', 'saachi.12@gmail.com', '+91 7708702844'),
-('Adharv', 'adharv@gmail.com', '+91 8631378676');
+('Saachi P', 'saachi.p@gmail.com', '+91 7708702844'),
+('Adharv M', 'm.adharv@gmail.com', '+91 8631378676'),
+('Anvi R' , 'anvi.r@gmail.com', '+91 97235476081');
 
 -- Seats Table
 INSERT INTO Seats (flight_id, seat_number) VALUES 
-(1, '1A'), (1, '1B'), (1, '2A'),
-(2, '1A'), (2, '1B');
+(1, '1A'),
+(1, '1B'),
+(1, '2A'),
+(2, '1A'),
+(2, '1B');
