2c92f78da44b0682a4831ed6c573aa6cad18c14d
Pujitha Medasani <mpujitha676@gmail.com>
2025-07-16 00:36:45 +0530

    Create book_seat.sql

---
+++ queries/book_seat.sql
@@ -0,0 +1,11 @@
+-- Book a Seat
+
+UPDATE Seats SET is_booked = TRUE WHERE seat_id = 1;
+
+INSERT INTO Bookings (customer_id, flight_id, seat_id)
+VALUES (1, 1, 1);
+
+-- This pair of queries simulates manual booking:
+-- At First , it marks a seat as booked (is_booked = TRUE). 
+-- Then, it inserts a new record into the Bookings table, linking a customer, flight, and seat together.
+
