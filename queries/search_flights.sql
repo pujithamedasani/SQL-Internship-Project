6e48820c482da1c4fae000e767f21fcc5c339403
Pujitha Medasani <mpujitha676@gmail.com>
2025-07-16 00:33:17 +0530

    Update search_flights.sql

--- queries/search_flights.sql
+++ queries/search_flights.sql
@@ -3,3 +3,6 @@
 SELECT * FROM Flights
 WHERE origin = 'New York' AND destination = 'London'
 AND departure_time BETWEEN '2025-08-01 00:00:00' AND '2025-08-01 23:59:59';
+
+-- This query searches for all flights departing from New York to London on August 1, 2025. It uses the BETWEEN clause to filter flights scheduled within the full day.
+
