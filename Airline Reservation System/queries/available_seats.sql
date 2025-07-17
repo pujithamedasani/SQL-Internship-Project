98c342243318018d63adee7b847bf548b8be332d
Pujitha Medasani <mpujitha676@gmail.com>
2025-07-18 00:21:44 +0530

    Create available_seats.sql

---
+++ Airline Reservation System/queries/available_seats.sql
@@ -0,0 +1,17 @@
+-- To Find Available Seats on a Flight
+
+SELECT seat_number
+FROM Seats
+WHERE flight_id = 1 AND is_booked = FALSE;
+
+-- This query retrieves all unbooked seat numbers for a specific flight (i.e., flight with ID = 1). It helps identify which seats are currently available for booking.
+
+Output:
+  
++-------------+
+| seat_number |
++-------------+
+| 1A          |
+| 1B          |
+| 2A          |
++-------------+
